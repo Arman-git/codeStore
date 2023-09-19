633bbaf353ce5c69332127bc95a8a3ae39613a03
Arman-git <73025140+Arman-git@users.noreply.github.com>
2023-09-19 22:39:24 +0600

    react-project

---
+++ React Pizza v2/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import "./scss/app.scss";
+import Header from "./Components/Header";
+import Categories from "./Components/Categories";
+import Sort from "./Components/Sort";
+import PizzaBlock from "./Components/PizzaBlock";
+
+
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+          <Categories />
+          <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+           <PizzaBlock title="Мексиканская" price={500}/>
+           <PizzaBlock title="Гавайская" price={300}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
