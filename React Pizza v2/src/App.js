24b214b9805d8c34845c335977c3db7d0a54fff2
Arman Tulyandiyev <ATulyandiyev@szhuldyz.kz>
2023-09-20 16:34:36 +0600

    123

--- React Pizza v2/src/App.js
+++ React Pizza v2/src/App.js
@@ -5,22 +5,20 @@ import Categories from "./Components/Categories";
 import Sort from "./Components/Sort";
 import PizzaBlock from "./Components/PizzaBlock";
 
-
-
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
-          <Categories />
-          <Sort />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-           <PizzaBlock title="Мексиканская" price={500}/>
-           <PizzaBlock title="Гавайская" price={300}/>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Гавайская" price={300} />
           </div>
         </div>
       </div>
--- React Pizza v2/src/Components/Categories.jsx
+++ React Pizza v2/src/Components/Categories.jsx
@@ -1,16 +1,20 @@
-function Categories() {
-    return (
-      <div class="categories">
-                <ul>
-                  <li class="active">Все</li>
-                  <li>Мясные</li>
-                  <li>Вегетарианская</li>
-                  <li>Гриль</li>
-                  <li>Острые</li>
-                  <li>Закрытые</li>
-                </ul>
-              </div>
-    )
-  }
+import React from 'react'
 
-  export default Categories;
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
