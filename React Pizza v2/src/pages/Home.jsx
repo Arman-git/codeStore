081d81d8438d96914e9ab15baa6bcff11fb063f9
Arman-git <73025140+Arman-git@users.noreply.github.com>
2023-09-25 22:31:05 +0600

    react-project2g1

--- React Pizza v2/src/pages/Home.jsx
+++ React Pizza v2/src/pages/Home.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import Categories from '../Components/Categories'
-import Sort from '../Components/Sort'
-import PizzaBlock from '../Components/PizzaBlock'
-import Skeleton from '../Components/PizzaBlock/Skeleton'
+import Categories from "../Components/Categories";
+import Sort from "../Components/Sort";
+import PizzaBlock from "../Components/PizzaBlock";
+import Skeleton from "../Components/PizzaBlock/Skeleton";
 
 const Home = () => {
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
-  
-    React.useEffect(() => {
-      fetch("https://650d2478a8b42265ec2bbc08.mockapi.io/items")
-        .then((res) => res.json())
-        .then((arr) => {
-          setItems(arr);
-          setIsLoading(false);
-        });
-    }, []);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://650d2478a8b42265ec2bbc08.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
-         <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading 
-             ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
-             : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
