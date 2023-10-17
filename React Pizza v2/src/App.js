0ba9d8b129e79159302a4465f49b8281f505a5e9
Arman Tulyandiyev <arman.tulyandiev@gmail.com>
2023-10-17 16:47:48 +0600

    fgggffgg

--- React Pizza v2/src/App.js
+++ React Pizza v2/src/App.js
@@ -3,6 +3,7 @@ import Header from "./Components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
+import FullPizza from "./pages/FullPizza";
 import { Routes, Route } from "react-router-dom";
 
 import "./scss/app.scss";
@@ -26,6 +27,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/pizza/:id" element={<FullPizza />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
---
+++ React Pizza v2/src/pages/FullPizza.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+
+const FullPizza = () => {
+    const params = useParams();
+    return (
+      <div className='container'>
+        <img src='' alt=""/>
+        <h2>2222</h2>
+        <p>The Sun is a British broadsheet newspaper, published by the News Group Newspapers division of News UK, itself a wholly owned subsidiary of Rupert Murdoch's News Corp. It was founded as a broadsheet in 1964 as a successor to the Daily Herald, and became a tabloid in 1969 after it was purchased by its current owner.</p>
+        <h4>250 ₽</h4>
+      </div>
+      
+    )
+}
+
+export default FullPizza
