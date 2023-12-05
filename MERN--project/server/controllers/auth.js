d05df69b47b8417338263dff0e946ccc2d924e9a
Arman-git <73025140+Arman-git@users.noreply.github.com>
2023-12-05 23:04:45 +0600

    yhyhy

---
+++ MERN--project/server/controllers/auth.js
@@ -0,0 +1,48 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+// Register user
+export const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const isUsed = await User.findOne({ username });
+
+    if (isUsed) {
+      return res.json({
+        message: "User already registered!",
+      });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hash,
+    });
+
+    await newUser.save();
+
+    res.json({
+      newUser,
+      message: "Регистрация прошла успешно!",
+    });
+  } catch (error) {
+    res.json({
+      message: "Ошибка при созданий пользователя!",
+    });
+  }
+};
+
+// Login user
+export const login = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+// Get Me
+export const getMe = async (req, res) => {
+  try {
+  } catch (err) {}
+};
