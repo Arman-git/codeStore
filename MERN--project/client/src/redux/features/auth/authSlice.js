9e4eb59677c22f92a5790a34f233b83131795ea4
Arman-git <73025140+Arman-git@users.noreply.github.com>
2023-12-07 22:54:20 +0600

    kikikik

--- MERN--project/client/src/redux/features/auth/authSlice.js
+++ MERN--project/client/src/redux/features/auth/authSlice.js
@@ -1,16 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "../../../utils/axios";
 
 const initialState = {
-    user: null, 
-    token: null, 
-    isLoading: false, 
-    status: null, 
-}
+  user: null,
+  token: null,
+  isLoading: false,
+  status: null,
+};
 
-export const authSlice = {
-    name: 'auth',
-    initialState,
-    reducers: {},
-}
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async ({ username, password }) => {
+    try {
+      const { data } = await axios.post("/auth/register", {
+        username,
+        password,
+      });
+      if (data.token) {
+        window.localStorage.setItem("token", data.token);
+      }
+      return data;
+    } catch (error) {
+      console.log(error);
+      // You may want to throw the error again to be caught by the rejectWithValue handler
+      throw error;
+    }
+  }
+);
 
-export default authSlice.reducer
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+        state.status = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.status = action.payload.message;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status = action.payload ? action.payload.message : "Rejected without payload";
+        state.isLoading = false;
+      });
+  },
+});
+
+export default authSlice.reducer;
