fe14c81ba6760d05c5e529462c4d3069e2dffb74
Arman Tulyandiyev <arman.tulyandiev@gmail.com>
2024-01-03 16:20:33 +0600

    login

--- react-employees/client/src/pages/login/index.tsx
+++ react-employees/client/src/pages/login/index.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../../components/layout";
 import { Card, Form, Row, Space, Typography } from "antd";
 import { CustomInput } from "../../components/custom-input";
 import { PasswordInput } from "../../components/password-input/input";
 import { CustomButton } from "../../components/custom-button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
-import { UserData } from "../../app/services/auth";
+import { UserData, useLoginMutation } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [loginUser, loginUserResult] = useLoginMutation();
+  const [error, setError] = useState("");
 
   const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
 
-  }
+      navigate("/");
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+        setError(err.data.message);
+      } else {
+        setError("Неизвестная ошибка!");
+      }
+    }
+  };
   return (
     <Layout>
       <Row align="middle" justify="center">
@@ -28,6 +45,7 @@ export const Login = () => {
             <Typography.Text>
               Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
