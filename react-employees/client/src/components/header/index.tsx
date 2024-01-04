873a2fdc46e38e43ff29fbee08ae5a98aac1526e
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-01-04 22:00:19 +0600

    addCustomer

--- react-employees/client/src/components/header/index.tsx
+++ react-employees/client/src/components/header/index.tsx
@@ -3,10 +3,22 @@ import styles from "./index.module.css";
 import { Layout, Space, Typography } from "antd";
 import { LoginOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
 import { CustomButton } from "../custom-button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../../features/auth/authSlice";
 
 export const Header = () => {
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onLogoutClick = () => {
+    dispatch(logout());
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <Layout.Header className={styles.header}>
       <Space>
@@ -17,18 +29,28 @@ export const Header = () => {
           </CustomButton>
         </Link>
       </Space>
-      <div className={styles.boxLeft}>
-        <Link to={Paths.register}>
-          <CustomButton type="ghost" icon={<UserOutlined />}>
-            Зарегистрироваться
-          </CustomButton>
-        </Link>
-        <Link to={Paths.login}>
-          <CustomButton type="ghost" icon={<LoginOutlined />}>
-            Войти
-          </CustomButton>
-        </Link>
-      </div>
+      {user ? (
+        <CustomButton
+          type="ghost"
+          icon={<LoginOutlined />}
+          onClick={onLogoutClick}
+        >
+          Выйти
+        </CustomButton>
+      ) : (
+        <Space>
+          <Link to={Paths.register}>
+            <CustomButton type="ghost" icon={<UserOutlined />}>
+              Зарегистрироваться
+            </CustomButton>
+          </Link>
+          <Link to={Paths.login}>
+            <CustomButton type="ghost" icon={<LoginOutlined />}>
+              Войти
+            </CustomButton>
+          </Link>
+        </Space>
+      )}
     </Layout.Header>
   );
 };
