ca7a08691c98d6d3754d60bca16df94a6ae05042
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-02-02 22:16:37 +0600

    schemas

--- hotel-management-next/schemas/index.ts
+++ hotel-management-next/schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from './user'
+import account from './account'
+import booking from './booking'
+import hotelRoom from './hotelRoom'
+
+export const schemaTypes = [user, account, booking, hotelRoom]
