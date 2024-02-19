6f88e0fb2b9985a9189227a79180914303fedf34
Arman Tulyandiyev <arman.tulyandiev@gmail.com>
2024-02-19 16:36:13 +0600

    payment

--- hotel-management-next/src/app/(web)/rooms/[slug]/page.tsx
+++ hotel-management-next/src/app/(web)/rooms/[slug]/page.tsx
@@ -11,6 +11,8 @@ import { GiSmokeBomb } from "react-icons/gi";
 import BookRoomCta from "@/app/components/BookRoomCta/BookRoomCta";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import axios from "axios";
+import { getStripe } from "@/libs/stripe";
 
 const RoomDetails = (props: { params: { slug: string } }) => {
   const {
@@ -40,7 +42,7 @@ const RoomDetails = (props: { params: { slug: string } }) => {
     return null;
   };
 
-  const handleBookNowClick = () => {
+  const handleBookNowClick = async () => {
     if (!checkinDate || !checkoutDate)
       return toast.error("Please provide checkin / checkout date");
 
@@ -51,7 +53,32 @@ const RoomDetails = (props: { params: { slug: string } }) => {
 
     const hotelRoomSlug = room.slug.current;
 
+    const stripe = await getStripe();
+
     //integrate Stripe
+    try {
+      const { data: stripeSession } = await axios.post("/api/stripe", {
+        checkinDate,
+        checkoutDate,
+        adults,
+        children: children,
+        numberOfDays,
+        hotelRoomSlug,
+      });
+
+      if (stripe) {
+        const result = await stripe.redirectToCheckout({
+          sessionId: stripeSession.id,
+        });
+
+        if (result.error) {
+          toast.error("Payment failed");
+        }
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+      toast.error("An error occured");
+    }
   };
 
   const calcNumDays = () => {
