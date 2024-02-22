36f6b76d686e2f11503e93c5615e0b269787e7db
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-02-22 23:14:15 +0600

    cannotfetch

---
+++ hotel-management-next/src/app/(web)/users/[id]/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { getUserBookings } from "@/libs/apis";
+import useSWR from "swr";
+
+const UserDetails = (props: { params: { id: string } }) => {
+  const {
+    params: { id: userId },
+  } = props;
+  const fetchUserBooking = async () => getUserBookings(userId);
+  const {
+    data: userBookings,
+    error,
+    isLoading,
+  } = useSWR("/api/userbooking", fetchUserBooking);
+
+  //   if (error) throw new Error("Cannot fetch data");
+  //   if (typeof userBookings === "undefined" && !isLoading)
+  //     throw new Error("Cannot fetch data");
+
+  console.log(userBookings);
+
+  return <div>UserDetails</div>;
+};
+
+export default UserDetails;
