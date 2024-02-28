61978baafbd16f5bb4bf8617d2834c2a57d13544
Arman Tulyandiyev <arman.tulyandiev@gmail.com>
2024-02-28 16:03:02 +0600

    docker

--- network-social/EXPRESS-API/routes/index.js
+++ network-social/EXPRESS-API/routes/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 
-const iploadDestination = "uploads";
+const uploadDestination = "uploads";
 
 //Показываем где хранить файлы
 const storage = multer.diskStorage({
-  destination: iploadDestination,
+  destination: uploadDestination,
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
+
+const uploads = multer({ storage: storage });
 
 router.get("/register", (req, res) => {
   res.send("register");
