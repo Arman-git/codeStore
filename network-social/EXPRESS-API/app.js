c7912fe2c64cd24d3a078d046e7d3ec7936ec1f3
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-03-01 11:20:56 +0500

    prisma

--- network-social/EXPRESS-API/app.js
+++ network-social/EXPRESS-API/app.js
@@ -3,24 +3,24 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const fs = require("fs");
 
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "jade");
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//Раздавать статические файлы из папки "uploads"
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api", require("./routes"));
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdir("uploads");
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
