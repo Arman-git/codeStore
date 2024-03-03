4999c9708d5632a352210045ac7194fb0cf09bcf
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-03-03 22:34:59 +0500

    error500

--- network-social/EXPRESS-API/routes/index.js
+++ network-social/EXPRESS-API/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const UserController = require("../controllers/user-controller.js");
+const UserController = require("../controllers");
 
 const uploadDestionation = "uploads";
 
