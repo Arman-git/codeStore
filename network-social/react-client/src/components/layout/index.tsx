8805e223e16b52d12b04ce01dcec2f2e804a3eaf
Arman Tulyandiyev <arman.tulyandiev@gmail.com>
2024-03-18 15:14:28 +0500

    posts

--- network-social/react-client/src/components/layout/index.tsx
+++ network-social/react-client/src/components/layout/index.tsx
@@ -1,9 +1,23 @@
 import { Header } from "../header"
 import { Container } from "../container"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import { NavBar } from "../nav-bar"
+// import { useSelector } from "react-redux"
+import { selectIsAuthenticated, selectUser } from "../../features/user/useSlice"
+import { useAppSelector } from "../../app/hooks"
+import { useEffect } from "react"
+import { Profile } from "../profile"
 
 export const Layout = () => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  const user = useAppSelector(selectUser)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/auth")
+    }
+  }, [])
   return (
     <>
       <Header />
@@ -13,6 +27,9 @@ export const Layout = () => {
         </div>
         <div className="flex-1 p-4">
           <Outlet />
+        </div>
+        <div className="flex-2 p-4">
+          <div className="flex-col flex gap-5">{!user && <Profile />}</div>
         </div>
       </Container>
     </>
