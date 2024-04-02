e140814e2d5d323aa03cc4c7771824a6c910fb15
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-04-02 20:22:25 +0500

    start

---
+++ next-app-rostelecom/rostelecom_shop/shop/app/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable max-len */
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Page</h1>
+    </main>
+  )
+}
