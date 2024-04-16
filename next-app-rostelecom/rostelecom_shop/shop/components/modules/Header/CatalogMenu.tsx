9adff1c40176ec811a4f533f4f9f9cf816b71b9f
Arman-git <73025140+Arman-git@users.noreply.github.com>
2024-04-16 17:09:04 +0500

    modal

---
+++ next-app-rostelecom/rostelecom_shop/shop/components/modules/Header/CatalogMenu.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const catalogMenu = () => {
+  const catalogMenuIsOpen = useStore()
+
+  return <div>catalogMenu</div>
+}
+
+export default catalogMenu
--- next-app-rostelecom/rostelecom_shop/shop/components/modules/MobileNavbar/MobileNavbar.tsx
+++ next-app-rostelecom/rostelecom_shop/shop/components/modules/MobileNavbar/MobileNavbar.tsx
@@ -1,15 +1,38 @@
 'use client'
-import { useLang } from '@/hooks/useLang'
 import Link from 'next/link'
+import {
+  closeCatalogMenu,
+  closeMenu,
+  openCatalogMenu,
+  openMenu,
+} from '@/context/madals'
+import { useLang } from '@/hooks/useLang'
+import { addOverflowHiddenToBody } from '@/lib/utills/common'
 
 const MobileNavbar = () => {
   const { lang, translations } = useLang()
+
+  const handleOpenMenu = () => {
+    addOverflowHiddenToBody()
+    openMenu()
+    closeCatalogMenu()
+  }
+
+  const handleOpenCatalogMenu = () => {
+    addOverflowHiddenToBody()
+    openCatalogMenu()
+    closeMenu()
+  }
+
   return (
     <div className='mobile-navbar'>
       <Link href='/' className='mobile-navbar__btn'>
         {translations[lang].breadcrumbs.main}
       </Link>
-      <button className='btn-reset mobile-navbar__btn'>
+      <button
+        className='btn-reset mobile-navbar__btn'
+        onClick={handleOpenCatalogMenu}
+      >
         {translations[lang].breadcrumbs.catalog}
       </button>
       <Link className='btn-reset mobile-navbar__btn' href='/favorites'>
@@ -18,7 +41,7 @@ const MobileNavbar = () => {
       <Link className='btn-reset mobile-navbar__btn' href='/cart'>
         {translations[lang].breadcrumbs.cart}
       </Link>
-      <button className='btn-reset mobile-navbar__btn'>
+      <button className='btn-reset mobile-navbar__btn' onClick={handleOpenMenu}>
         {translations[lang].breadcrumbs.more}
       </button>
     </div>
